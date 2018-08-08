003947249e92244a3932b500cb5419db8f8ce5b8
梁斌斌 <bbliang@today36524.com.cn>
2018-08-08 15:51:43 +0800

    add eventType

--- agent_server-api/src/main/scala/com/github/dapeng/socket/enums/EventType.scala
+++ agent_server-api/src/main/scala/com/github/dapeng/socket/enums/EventType.scala
@@ -32,31 +32,39 @@ object EventType {
   val GET_SERVER_TIME = new EventType(6, "getServerTime")
   //部署事件
   val DEPLOY = new EventType(7, "deploy")
+  val DEPLOY_RESP = new EventType(8, "deployResp")
   //停止服务事件
-  val STOP = new EventType(8, "stop")
-  val RESTART = new EventType(9, "restart")
+  val STOP = new EventType(9, "stop")
+  val STOP_RESP = new EventType(10, "stopResp")
+  val RESTART = new EventType(11, "restart")
+  val RESTART_RESP = new EventType(12, "restartResp")
   //获取yamlFile事件
-  val GET_YAML_FILE = new EventType(10, "getYamlFile")
+  val GET_YAML_FILE = new EventType(13, "getYamlFile")
   //获取yamlFile resp
-  val GET_YAML_FILE_RESP = new EventType(11, "getYamlFileResp")
-  val GET_SERVICE_STATUS = new EventType(12, "getServiceStatus")
-  val GET_SERVICE_STATUS_RESP = new EventType(13, "getServiceStatusResp")
+  val GET_YAML_FILE_RESP = new EventType(14, "getYamlFileResp")
+  val GET_SERVICE_STATUS = new EventType(15, "getServiceStatus")
+  val GET_SERVICE_STATUS_RESP = new EventType(16, "getServiceStatusResp")
   val ERROR_EVENT = new EventType(99, "errorEvent")
 
   def unknown(id: Int) = new EventType(id, id + "")
 
   def valueOf(id: Int): EventType = id match {
-    case 2 => NODE_REG
-    case 3 => NODE_EVENT
-    case 4 => WEB_REG
-    case 5 => WEB_EVENT
-    case 7 => GET_SERVER_TIME_RESP
-    case 8 => GET_SERVER_TIME
-    case 9 => DEPLOY
-    case 10 => GET_YAML_FILE
-    case 11 => GET_YAML_FILE_RESP
-    case 12 => GET_SERVICE_STATUS
-    case 13 => GET_SERVICE_STATUS_RESP
+    case 1 => NODE_REG
+    case 2 => NODE_EVENT
+    case 3 => WEB_REG
+    case 4 => WEB_EVENT
+    case 5 => GET_SERVER_TIME_RESP
+    case 6 => GET_SERVER_TIME
+    case 7 => DEPLOY
+    case 8 => DEPLOY_RESP
+    case 9 => STOP
+    case 10 => STOP_RESP
+    case 11 => RESTART
+    case 12 => RESTART_RESP
+    case 13 => GET_YAML_FILE
+    case 14 => GET_YAML_FILE_RESP
+    case 15 => GET_SERVICE_STATUS
+    case 16 => GET_SERVICE_STATUS_RESP
     case 99 => ERROR_EVENT
     case _ => unknown(id)
   }
