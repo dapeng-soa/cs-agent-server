ec83fdfec7f7d2eb901216d211e8fbd1fbcdeab3
StruggleYang <yq1724555319@gmail.com>
2018-08-29 15:30:14 +0800

    fix update

--- startup.sh
+++ startup.sh
@@ -1,9 +1,11 @@
 oldpid=`cat pid.txt`
 echo killing Previous pid: ${oldpid}
 kill ${oldpid}
+sleep 10s
 echo runing app
-java -jar agent_server.jar >> ./logs/agent_server.log 2>&1 &
+nohup java -jar agent_server.jar >/dev/null &
 echo "$!" > pid.txt
 newpid=`cat pid.txt`
 echo runing new app pid is : ${newpid}
+
 
